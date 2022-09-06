2015e7eadb63965fd74dcb0ebc836588f2b09738
Chris Johnson <Johnny.C.Alexander@gmail.com>
2022-09-06 17:10:19 -0500

    able to sub numbers for white space where appropriate. Need to add rule math for mines

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,3 +4,30 @@ Write your code for the 'Minesweeper' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/minesweeper` directory.
 =end
+
+def minesweeper(board)
+count = 0
+
+answer = board.each do |spaces| 
+    spaces.split('').each { |space|
+        if (space == "*") 
+            puts spaces[space].replace(count.to_s)
+            # puts spaces
+            # spaces[c].gsub!("*", "f")
+            # puts spaces
+        end
+    } 
+end
+puts answer
+end
+
+# def count_between arr, lower, upper
+#     return 0 if lower > upper
+#     arr.count{|v| (lower..upper).include?(v)}
+#   end
+
+minesweeper(['+------+', '| *  * |', '|  *   |', '|    * |', '|   * *|',
+        '| *  * |', '|      |', '+------+'])
+
+        # minesweeper(['+------+', '|1*22*1|', '|12*322|', '| 123*2|', '|112*4*|',
+        #     '|1*22*2|', '|111111|', '+------+'])
