84894bb438d02300cb20ab054a239135942d6649
Chris Johnson <Johnny.C.Alexander@gmail.com>
2022-09-09 14:40:28 -0500

    added thought process to minesweeper.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,33 +1,67 @@
-=begin
-Write your code for the 'Minesweeper' exercise in this file. Make the tests in
-`minesweeper_test.rb` pass.
-To get started with TDD, see the `README.md` file in your
-`ruby/minesweeper` directory.
-=end
+# build coordinate array by interating over input
+# iterate over each each row assigning x axis value
+# iterate over rows again with row[x] counting y axis
+# if * any adjacents at the * coordinate defined as [x1 - x2] > 1 || [y1 - y2] >1 incrememt that coordinate by 1
 
-def minesweeper(board)
-count = 0
+# might try # array.repeated_permutation(int).map{|i,j| "#{i}-#{j}"} to build coordinates
+# might try using next if row in item isn't white space, int, or * to avoid border
 
-answer = board.each do |spaces| 
-    spaces.split('').each { |space|
-        if (space == "*") 
-            puts spaces[space].replace(count.to_s)
-            # puts spaces
-            # spaces[c].gsub!("*", "f")
-            # puts spaces
+
+class Board
+    def self.transform(board)
+    
+        removeBorder(board).each do |rows|
+            rows.chars.each_with_index do |spaceInRow, index|
+                puts spaceInRow[0]
+                rowNum += 1
+                if (spaceInRow == "*")
+                    
+                    spaceInRow[index - 1] = "1" unless spaceInRow[index - 1].nil?
+                    # spaceInRow[index + 1] = "1"
+                    
+                    # puts spaceInRow[index - 1] = rowNum.to_s and spaceInRow[index + 1] = rowNum.to_s
+                end
+                
+            end
+        
         end
-    } 
-end
-puts answer
+
+        puts board
+    end
+
+   
 end
 
-# def count_between arr, lower, upper
-#     return 0 if lower > upper
-#     arr.count{|v| (lower..upper).include?(v)}
-#   end
+def removeBorder(board)
+    board.shift
+    board.pop
+    board.each do |spaces|
+        spaces.slice!(0)
+        spaces.chop!
 
-minesweeper(['+------+', '| *  * |', '|  *   |', '|    * |', '|   * *|',
+    end
+end
+
+
+ # board.each do |rowOfBoard| 
+            # # puts currentRow = "current row is #{rowOfBoard}" #will print individual row as they come in
+            #     modifiedRow = rowOfBoard.split('').each_with_index do |space, index|
+            # count = 0;
+            # if (space == "*") 
+                
+            #     left = index - 1
+            #     right = index + 1
+    
+            # left and right positions of a * but doesn't catch adjacents/gets hung up on border
+            #     # puts rowOfBoard[left] = count += 1
+            #     # puts rowOfBoard[right] = count += 1
+                
+                
+            #     puts space[left] = "1"
+                # puts "space is #{space} index is #{index}"
+        
+
+
+# testing without suite
+Board.transform(['+------+', '| *  * |', '|  *   |', '|    * |', '|   * *|',
         '| *  * |', '|      |', '+------+'])
-
-        # minesweeper(['+------+', '|1*22*1|', '|12*322|', '| 123*2|', '|112*4*|',
-        #     '|1*22*2|', '|111111|', '+------+'])
