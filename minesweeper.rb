6c0d9526d34b7af2a54fe6e019c182c1b52636d4
Chris Johnson <Johnny.C.Alexander@gmail.com>
2022-09-10 00:49:33 -0500

    returning transformed board but count is off

--- minesweeper.rb
+++ minesweeper.rb
@@ -8,93 +8,44 @@
 
 class Board
     def self.transform(board)
-
-      workBoard = removeBorder(board)
       
+      mineCoordinates = []
+    puts board
+      workBoard = removeBorder(board)
       workBoard.each_with_index do |row, x|
-        puts "rows #{row.split('')}, #{x}"
+        # puts "rows #{row.split('')}, #{x}"
         row.split('').each_with_index do |column, y|
-             puts "columns #{column}, #{y}"
-             if (workBoard[x][y] == "*")
-                puts "found at #{x}#{y}"
+          # puts "columns #{column}, #{y}"
+          
+          if (workBoard[x][y] == "*")
+            mineCount = 0
+            mineCoordinates.push([x,y])
+            workBoard[x + 1][y] = (mineCount += 1).to_s
+            workBoard[x][y + 1] = (mineCount += 1).to_s
+            workBoard[x - 1][y] = (mineCount + 1).to_s
+            workBoard[x][y - 1] = (mineCount + 1).to_s
+  
           end
         end
-        #   column.split('')
-        #   puts "rows #{column}, #{y}"
-        # end
-        # row.to_a.each_with_index do |x, xindex|
-        #   puts "rows #{x}, #{xindex}"
-        # row.split('').each_with_index do |spaceInRowX, x|
-          # puts " this is row #{spaceInRowX}#{x}"
-          # spaceInRowX.each_with_index do |_spaceInRowY, y|
-            
-          #           puts "row at x #{_spaceInRowY}#{y}"
-          # end
+        
         end
-        #   
-        #     puts "this is xcoords #{row}#{x} "
-        #     puts "this is ycoords #{cell}#{y} "
-        #   end
+      mineCoordinates.each do |mine|
+            puts "mine here, #{mine}"
+          end
+          puts board
+       # [x1 - x2] > 1 || [y1 - y2] >1
+  end
+      end
   
-        # rowItem = row.split('')
+  def removeBorder(board)
+      board.shift
+      board.pop
+      board.each do |spaces|
+          spaces.slice!(0)
+          spaces.chop!
+  
       end
   end
-
-# class Board
-#     def self.transform(board)
-    
-#         removeBorder(board).each do |rows|
-#             rows.chars.each_with_index do |spaceInRow, index|
-#                 puts spaceInRow[0]
-#                 rowNum += 1
-#                 if (spaceInRow == "*")
-                    
-#                     spaceInRow[index - 1] = "1" unless spaceInRow[index - 1].nil?
-#                     # spaceInRow[index + 1] = "1"
-                    
-#                     # puts spaceInRow[index - 1] = rowNum.to_s and spaceInRow[index + 1] = rowNum.to_s
-#                 end
-                
-#             end
-        
-#         end
-
-#         puts board
-#     end
-
-   
-# end
-
-def removeBorder(board)
-    board.shift
-    board.pop
-    board.each do |spaces|
-        spaces.slice!(0)
-        spaces.chop!
-
-    end
-end
-
-
- # board.each do |rowOfBoard| 
-            # # puts currentRow = "current row is #{rowOfBoard}" #will print individual row as they come in
-            #     modifiedRow = rowOfBoard.split('').each_with_index do |space, index|
-            # count = 0;
-            # if (space == "*") 
-                
-            #     left = index - 1
-            #     right = index + 1
-    
-            # left and right positions of a * but doesn't catch adjacents/gets hung up on border
-            #     # puts rowOfBoard[left] = count += 1
-            #     # puts rowOfBoard[right] = count += 1
-                
-                
-            #     puts space[left] = "1"
-                # puts "space is #{space} index is #{index}"
-        
-
-
-# testing without suite
-Board.transform(['+------+', '| *  * |', '|  *   |', '|    * |', '|   * *|',
-        '| *  * |', '|      |', '+------+'])
+  
+  Board.transform(['+------+', '| *  * |', '|  *   |', '|    * |', '|   * *|', '| *  * |', '|      |', '+------+'])
+  
